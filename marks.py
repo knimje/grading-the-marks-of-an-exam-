f2561b560f82f671224d30a9b07e660021b903a6
knimje <124028519+knimje@users.noreply.github.com>
2023-04-12 21:00:33 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,15 @@
+mark=int(input("Enter marks "))
+if mark==100:
+    print("outstanding")
+elif mark>90:
+    print("A+")
+elif mark>80:
+    print("A")
+elif mark>70:
+    print("B+")
+elif mark>60:
+    print("B")
+elif mark>50:
+    print("C")
+else:
+    print("work hard")
